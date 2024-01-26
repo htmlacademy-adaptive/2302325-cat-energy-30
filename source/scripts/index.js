101d311206ece571706b393cb1715c2306968767
Татьяна <stepanova.airbag@gmail.com>
2024-01-27 02:43:46 +0300

    правки главной страницы мобильной версии

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,10 @@
 /* в этот файл добавляет скрипты*/
+if (document.querySelector('.nojs')) {
+  document.querySelector('.nojs').classList.remove('.nojs');
+}
+
 const menu = document.querySelector('.page-header__menu');
-const menuBtn = document.querySelector('.button-toggle');
+const menuBtn = document.querySelector('.page-header__toggle');
 
 menuBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
