4a92957d58e0645fa1a92fb9a285552c9f65ae14
Татьяна <stepanova.airbag@gmail.com>
2024-01-30 12:45:51 +0300

    доработка проекта. пиксель

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,12 +1,14 @@
 /* в этот файл добавляет скрипты*/
 if (document.querySelector('.nojs')) {
-  document.querySelector('.nojs').classList.remove('.nojs');
+  document.querySelector('.nojs').classList.remove('nojs');
 }
 
-const menu = document.querySelector('.page-header__menu');
+const header = document.querySelector('.page-header__menu');
+const menu = document.querySelector('.nav');
 const menuBtn = document.querySelector('.page-header__toggle');
 
 menuBtn.addEventListener('click', (evt) => {
   evt.preventDefault();
-  menu.classList.toggle('is-open');
+  header.classList.toggle('page-header__menu--opened');
+  menu.classList.toggle('nav--closed');
 });
